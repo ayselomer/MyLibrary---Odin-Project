3418b1b86144d4f77819441cba2f6f29ca245f38
ayselomer <ayselomer@gmail.com>
2024-07-24 13:56:15 +0500

    made a button to add books to the library array, and form clears

--- library/script.js
+++ library/script.js
@@ -13,5 +13,40 @@ function showform() {
 }
 
 
-//creating new books
+//creating an array for the library
+
+const myLibrary = [];
+
+//book constructor, all the new books will have this property
+
+function Book(title, author, status) {
+    this.title = title;
+    this.author = author;
+    this.status = status;
+}
+
+//button for adding to library
+
+let addToLibrary = document.getElementById('addToLibrary')
+addToLibrary.addEventListener('click', addBookToLibrary)
+
+//to add books to the library array
+
+function addBookToLibrary(event) {
+    event.preventDefault();
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const status = document.getElementById('status').value
+
+    const newBook = new Book(title, author, status)
+
+    myLibrary.push(newBook);
+    console.log(myLibrary)
+
+    document.getElementById('bookform').reset()
+
+
+}
+
+
 
